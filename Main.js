d6bf1940b3a5e989a0e41c458b7923e6658c4cf7
naaam-h-siddhu <sudhakarsingh2004@gmail.com>
2025-03-07 01:56:21 +0530

    ✨ [Sudhakar] Add. Added support of storing multiple address in array

--- Main.js
+++ Main.js
@@ -1,4 +1,26 @@
 import {AddressBookContact} from "./AddressBookContact.js";
 
-let contact1 = new AddressBookContact("Sudhakar", "Singh","2A", "Bhopal", "Madhya Pradesh",462041,48892323242,"sudhakar@example.com")
-console.log(contact1)
+class AddressBook{
+  constructor(){
+    this.contacts =  [];
+  }
+  addContact(contact){
+    this.contacts.push(contact);
+    console.log("Successfully added the contact");
+  }
+  displayContact(){
+    console.log("Address Book Contact");
+    this.contacts.forEach((contact, index) => console.log(`${index + 1}. ${contact.toString()}`));
+
+  }
+}
+
+const addressBook = new AddressBook();
+
+let contact1 = new AddressBookContact("Sudhakar", "Singh","201A", "Bhopal", "Madhya Pradesh",462041,4889323242,"sudhakar@example.com")
+let contact2 = new AddressBookContact("Siddhu","Singh","456 Avenue","Bhopal","Madhya Pradesh",462021,4242556421,"siddhu@example.com")
+
+addressBook.addContact(contact1)
+addressBook.addContact(contact2)
+
+addressBook.displayContact()
